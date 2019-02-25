b94b75124362bf05e81dd32e9b4a65df52b6afff
zhangpeng <503460985@qq.com>
2019-02-25 13:53:25 +0800

    完善关于类类型的代码注释规范

--- ppclass/ClassDemo.java
+++ ppclass/ClassDemo.java
@@ -1,25 +1,31 @@
 package ppclass;
 
 /**
+ * 根据类获取 类类型         说明每个类都有个隐藏的静态成员变量
+ * 根据类对象获取 类类型
+ * 根据类名称获取 类类型
+ *
  * 类类型的相关说明
  */
 public class ClassDemo {
     public static void main(String[] args) {
         Foo foo = new Foo();
 
-        //Foo这个类也是个实例对象，Class类的实例对象，但是Class这个类的构造方法是私有的
-        Class c1 = Foo.class;                       //根据类获取 类类型         说明每个类都有个隐藏的静态成员变量
-        Class c2 = foo.getClass();                  //根据类对象获取 类类型
-        Class c3 = null;                            //根据类名称获取 类类型
+        // Foo这个类也是个实例对象，Class类的实例对象，但是Class这个类的构造方法是私有的
+        Class c1 = Foo.class;
+        Class c2 = foo.getClass();
+        Class c3 = null;
         try {
-            c3 = Class.forName("ppclass.Foo");      //动态加载类，运行时加载
+            // 动态加载类，运行时加载
+            c3 = Class.forName("ppclass.Foo");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
 
-        //可以根据类类型 创建实例对象
+        // 可以根据类类型 创建实例对象
         try {
-            Foo temp = (Foo) c1.getDeclaredConstructor().newInstance(); //获取类的实例对象
+            // 获取类的实例对象
+            Foo temp = (Foo) c1.getDeclaredConstructor().newInstance();
         } catch (Exception e) {
             e.printStackTrace();
         }
